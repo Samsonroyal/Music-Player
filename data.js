a15b8c65eff645b6aa27cec57e71651372057793
Samson Royal <werefast2000@gmail.com>
2021-09-06 23:29:10 +0300

    added js files for data(audio) and controls

---
+++ data.js
@@ -0,0 +1,61 @@
+let songs = [{
+        name: 'Counting on you',
+        path: 'musics/counting on you.mp3',
+        artist: 'Zulitums',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'Meant for me',
+        path: 'musics/meant for me.mp3',
+        artist: 'Zulitums',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'One minute',
+        path: 'musics/One minute.mp3',
+        artist: 'Zulitums',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: 'Somebody daughter',
+        path: 'musics/Somebody Daughter.mp3',
+        artist: 'Zulitums',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'Tabu',
+        path: 'musics/Tabu.mp3',
+        artist: 'Zulitums',
+        cover: 'images/cover 5.png'
+    },
+    {
+        name: 'Sinner',
+        path: 'musics/Adekunle Gold feat Lucky Daye - Sinner (Official Audio).mp3',
+        artist: 'Adekunle Gold',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'Amen',
+        path: 'musics/Amen by Chike lyrics.mp3',
+        artist: 'Chike',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'Brown Skin Girl',
+        path: 'musics/Beyonce, Blue Ivy - Brown Skin Girl (Lyrics) Ft. Wizkid & Saint Jhn.mp3',
+        artist: 'Beyonce ft Blue Ivy ft Wizkid & Saint Jhn',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'On Fire',
+        path: 'musics/ANDY BUMUNTU - ON FIRE (OFFICIAL VIDEO).mp3',
+        artist: 'Andy Bumuntu',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'If you no love',
+        path: 'musics/Chiké - If You No Love (Official Audio).mp3',
+        artist: 'Chike',
+        cover: 'images/cover 2.png'
+    }
+]
